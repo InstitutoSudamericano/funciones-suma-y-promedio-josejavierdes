2bc2977d5f893b4be721144bcf90173bc624eb77
embenavides <xdmateo90@gmail.com>
2023-07-14 10:44:03 -0500

    Tarea S14

---
+++ index2.js
@@ -0,0 +1,24 @@
+let firstGrade = Number(prompt("Escribe la primera nota:"));
+let secondGrade = Number(prompt("Escribe la segunda nota:"));
+let thirdGrade = Number(prompt("Escribe la tercera nota:"));
+
+function checkIfApproved(a, b, c){
+    let sum = a+b+c;
+    let average = sum/3;
+
+    if (average>=70){
+
+        document.getElementById("text2").style.color = "green";
+        return "APROBADO"
+    }
+    return "REPROBADO"
+}
+
+let result =  checkIfApproved(firstGrade, secondGrade, thirdGrade);
+
+
+document.getElementById("text").textContent = "Tus notas son:";
+
+
+document.getElementById("text2").textContent = result;
+
